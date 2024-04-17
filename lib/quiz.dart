64cbbf688f4bf9d98a301de4645a9571fe929f85
balayogi789 <balyogigmcs@gmail.com>
2024-04-17 14:05:54 -0400

    liftstateup quiz and questions_screen dart files

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:quizapp/data/questions.dart';
 import 'package:quizapp/questions_screen.dart';
 import 'package:quizapp/start_screen.dart';
 
@@ -13,6 +14,11 @@ class Quiz extends StatefulWidget {
 
 class _QuizState extends State<Quiz> {
   var activeScreen = 'start-screen';
+  final List<String> selectedAnswers = [];
+
+  void choosenAnswer(answer){
+    selectedAnswers.add(answer);
+  }
 
   void switchScreen() {
     setState(() {
@@ -34,7 +40,7 @@ class _QuizState extends State<Quiz> {
           ),
           child: activeScreen == 'start-screen'
               ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+              :  QuestionsScreen(onSelectAnswer: choosenAnswer,),
         ),
       ),
     );
